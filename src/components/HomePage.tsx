eda7018c0516c1304bcbddfd73a8bf82c5a9da0b
Igor Gabriel Vieira Chaves <contato.igorgabriel@hotmail.com>
2025-01-31 18:31:03 -0300

    ui: Page header
    Summary:
    - Adding SVG
    - Developing the page header

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,8 +1,20 @@
+import svg from "~assets/svg/line.svg";
 import { TodoListStateProvider } from "./TodoListStateProvider";
 
 export const HomePage = () => {
     return (
         <section className="w-full h-auto min-h-screen flex flex-col bg-neutral-950">
+            <header className="w-full h-min pt-10 px-4 pb-2 flex flex-col gap-y-2 items-center">
+                <h1 className="w-full h-min text-purple-600 font-patrickHand text-6xl text-center md:text-8xl">
+                    ToDo List
+                </h1>
+                <img
+                    src={svg}
+                    alt="Line"
+                    loading="lazy"
+                    className="w-full max-w-lg h-min object-contain lg:max-w-4xl"
+                />
+            </header>
             <TodoListStateProvider>
                 <h1 className="w-full h-min text-neutral-50 font-patrickHand text-6xl text-center">App</h1>
             </TodoListStateProvider>
